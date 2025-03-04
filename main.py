b9fd159ea61e593df6c46fc8c3f7dbf10ada93e9
Kyle <kylesch115@gmail.com>
2025-03-03 22:02:45 -0700

    setup venv

---
+++ main.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+def main():
+    print("Back to Python")
+
+    arr = np.array([1, 2, 3, 4, 5])
+    for x in arr:
+        print(x)
+
+
+if __name__ == "__main__":
+    main()
